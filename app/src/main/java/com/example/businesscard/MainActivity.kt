2a226d26e680baabcbb9057186ecf5491b9a3ef5
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-04-21 16:59:50 +0530

    commit

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -5,22 +5,30 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontStyle
+
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
+import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,23 +53,41 @@ class MainActivity : ComponentActivity() {
 //    Text(text = "Hello $name!")
 //}
 fun BusinessCard(){
-    Column(modifier = Modifier.fillMaxSize().background(Color.DarkGray)) {
-        Column() {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .background(Color.DarkGray),verticalArrangement = Arrangement.SpaceEvenly) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
+
+            ) {
             //Section 1
             val logo = painterResource(id = R.drawable.android_logo)
-            Image(painter = logo, contentDescription = null, modifier = Modifier.padding(16.dp))
+            Image(painter = logo, contentDescription = null, modifier = Modifier
+                .padding(start = 16.dp, end = 16.dp)
+                .fillMaxWidth().height(100.dp))
 
             Text(
-                text = "Title",
+                text = "Full Name",
                 fontSize = 22.sp,
                 fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(16.dp),
-                color = Color.White
+                modifier = Modifier.padding(start = 16.dp,end = 16.dp),
+                color = Color.White,
+            )
+            Text(
+                text = "Title"
+                    , color = Color.White,
+                modifier = Modifier.padding(start = 16.dp,end = 16.dp)
             )
 
+
         }
-        Column() {
+        Column(horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxWidth()
+            ) {
             //Section2
+            Row() {
+                Text(text = "Text")
+            }
         }
     }
 }
