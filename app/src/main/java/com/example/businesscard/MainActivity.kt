141b9fe99bddc9778b54583b158cfd3cb3eb2ddd
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-04-21 12:09:02 +0530

    commit

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -3,13 +3,23 @@ package com.example.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.businesscard.ui.theme.BusinessCardTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +30,10 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    color = Color.DarkGray
                 ) {
-                    Greeting("Android")
+//                    Greeting("Android")
+                    BusinessCard()
                 }
             }
         }
@@ -30,14 +41,36 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+//fun Greeting(name: String) {
+//    Text(text = "Hello $name!")
+//}
+fun BusinessCard(){
+    Column(modifier = Modifier.fillMaxSize().background(Color.DarkGray)) {
+        Column() {
+            //Section 1
+            val logo = painterResource(id = R.drawable.android_logo)
+            Image(painter = logo, contentDescription = null, modifier = Modifier.padding(16.dp))
+
+            Text(
+                text = "Title",
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold,
+                modifier = Modifier.padding(16.dp),
+                color = Color.White
+            )
+
+        }
+        Column() {
+            //Section2
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     BusinessCardTheme {
-        Greeting("Android")
+//        Greeting("Android")
+        BusinessCard()
     }
 }
