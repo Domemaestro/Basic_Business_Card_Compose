173414b1b929244e130cb1274edb1d80a6934f87
Durgesh Pawar <durgeshhulkiron@gmail.com>
2023-04-21 20:14:37 +0530

    commit

--- app/src/main/java/com/example/businesscard/MainActivity.kt
+++ app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -14,9 +14,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -38,7 +43,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = Color.DarkGray
+                    color = MaterialTheme.colors.background
                 ) {
 //                    Greeting("Android")
                     BusinessCard()
@@ -64,29 +69,50 @@ fun BusinessCard(){
             val logo = painterResource(id = R.drawable.android_logo)
             Image(painter = logo, contentDescription = null, modifier = Modifier
                 .padding(start = 16.dp, end = 16.dp)
-                .fillMaxWidth().height(100.dp))
+                .fillMaxWidth()
+                .height(100.dp))
 
             Text(
                 text = "Full Name",
-                fontSize = 22.sp,
-                fontWeight = FontWeight.Bold,
+                fontSize = 30.sp,
                 modifier = Modifier.padding(start = 16.dp,end = 16.dp),
                 color = Color.White,
             )
             Text(
                 text = "Title"
                     , color = Color.White,
+                fontWeight = FontWeight.Bold,
                 modifier = Modifier.padding(start = 16.dp,end = 16.dp)
             )
 
 
         }
-        Column(horizontalAlignment = Alignment.CenterHorizontally,
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.fillMaxWidth()
             ) {
             //Section2
             Row() {
-                Text(text = "Text")
+                Icon(imageVector = Icons.Filled.Call, contentDescription = null,
+                modifier = Modifier.padding(12.dp))
+                Text(text = "Text",
+                    modifier = Modifier.padding(12.dp)
+                )
+            }
+            Row() {
+                Icon(imageVector = Icons.Filled.Share,
+                    contentDescription = null,
+                modifier = Modifier.padding(12.dp))
+                Text(text = "Text2",
+                    modifier = Modifier.padding(12.dp)
+                    )
+            }
+            Row() {
+                Icon(imageVector = Icons.Filled.Email,
+                    contentDescription = null,
+                modifier = Modifier.padding(12.dp))
+                Text(text = "text3",
+                modifier = Modifier.padding(12.dp))
             }
         }
     }
